125859e5d1d4e5f24722a9d751c87c30a027c4b7
dbellonzi <dbellonzi@aol.com>
2018-04-12 16:06:51 -0700

    removed about section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
-import { AboutComponent } from './about/about.component';
 import { HeaderComponent } from './header/header.component';
 import { FaqComponent } from './faq/faq.component';
 import { MapComponent } from './map/map.component';
@@ -27,7 +26,6 @@ import { SponsorEachComponent } from './sponsor-each/sponsor-each.component';
   declarations: [
     AppComponent,
     FooterComponent,
-    AboutComponent,
     HeaderComponent,
     FaqComponent,
     MapComponent,
