b561c2316858f5acb8284fc5247c9dd9f7d6b1ac
Keith Nguyen <khoacn@gmail.com>
2018-04-12 15:01:37 -0700

    updated map comp

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -372,7 +372,7 @@ export class MapComponent implements OnInit {
           },
         }, //end of source
         "paint": {
-          "fill-color": "#FD3468",
+          "fill-color": "#12CBC4",
           "fill-opacity": 0.8
         }
       }); //end of exhibits layer
@@ -627,7 +627,7 @@ export class MapComponent implements OnInit {
           },
         }, //end of source
         "paint": {
-          "fill-color": "#6ADC07",
+          "fill-color": "#16992F",
           "fill-opacity": 0.8
         }
       }); //end of sponsors layer
@@ -1004,7 +1004,7 @@ export class MapComponent implements OnInit {
           },
         }, //end of source
         "paint": {
-          "fill-color": "#B806AD",
+          "fill-color": "#EB4267",
           "fill-opacity": 0.8
         }
       }); //end of stages layer
@@ -1046,10 +1046,97 @@ export class MapComponent implements OnInit {
         },
         'layout': {},
         'paint': {
-          'fill-color': '#FE5D4D',
+          'fill-color': '#B53471',
           'fill-opacity': 0.8
         }
       }); // end of ninolandia layer
+
+      this.map.addLayer({ //health and wellness pavilion
+        'id': 'health and wellness',
+        'type': 'fill',
+        'source': {
+          'type': 'geojson',
+          'data': {
+            'type': 'Feature',
+            'geometry': {
+              'type': 'Polygon',
+              "coordinates": [
+                [
+                  [
+                    -122.41203256503341,
+                    37.75420281592962
+                  ],
+                  [
+                    -122.41201180207322,
+                    37.75399878673228
+                  ],
+                  [
+                    -122.41195247934553,
+                    37.754005822229004
+                  ],
+                  [
+                    -122.41197324229994,
+                    37.75420750624788
+                  ],
+                  [
+                    -122.41203256503341,
+                    37.75420281592962
+                  ]
+                ]
+              ]
+            }
+          }
+        },
+        'layout': {},
+        'paint': {
+          'fill-color': '#0652DD',//blue
+          'fill-opacity': 0.8
+        }
+      }); // end of health and wellness
+
+      this.map.addLayer({ //Native and African American Exhibit
+        'id': 'Native and African American Exhibit',
+        'type': 'fill',
+        'source': {
+          'type': 'geojson',
+          'data': {
+            'type': 'Feature',
+            'geometry': {
+              'type': 'Polygon',
+              "coordinates": [
+                [
+                  [
+                    -122.41200747234609,
+                    37.75398060422596
+                  ],
+                  [
+                    -122.41199171555563,
+                    37.75381366605424
+                  ],
+                  [
+                    -122.41193656679215,
+                    37.75381615767077
+                  ],
+                  [
+                    -122.41195389926104,
+                    37.75398683325312
+                  ],
+                  [
+                    -122.41200747234609,
+                    37.75398060422596
+                  ]
+                ]
+              ]
+            }
+          }
+        },
+        'layout': {},
+        'paint': {
+          'fill-color': '#1B1464',
+          'fill-opacity': 0.8
+        }
+      }); // End Native and African American Exhibit
+
       this.map.addLayer({ //beverages layer
         "id": "beverages",
         "type": "line",
@@ -1153,6 +1240,7 @@ export class MapComponent implements OnInit {
           "line-opacity": 0.8
         }
       }); // end of beverages layer
+
       this.map.addLayer({ //foodboth layer
         "id": "food-booth",
         "type": "fill",
@@ -1269,10 +1357,11 @@ export class MapComponent implements OnInit {
           },
         },
         "paint": {
-          "fill-color": "#00B1FC",
+          "fill-color": "#F79F1F",
           "fill-opacity": 0.8
         }
       }); //end of food-booth layer
+
       this.map.addLayer({ //foodtruck layer
         "id": "food-truck",
         "type": "line",
@@ -1376,7 +1465,7 @@ export class MapComponent implements OnInit {
           "line-cap": "round"
         },
         "paint": {
-          "line-color": "#FE4101",
+          "line-color": "#EA2027",
           "line-width": 5
         }
       }); // end of food-truck layer
@@ -1470,7 +1559,7 @@ export class MapComponent implements OnInit {
         },
         'layout': {},
         'paint': {
-          'fill-color': '#F91405',
+          'fill-color': '#EE5A24',
           'fill-opacity': 0.8
         }
       }); // end of lowriders layer
