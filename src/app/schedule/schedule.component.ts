dae52f6f775195743dc4c6526e1ff819759e5742
Keith Nguyen <khoacn@gmail.com>
2018-05-09 10:25:19 -0700

    update

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -13,44 +13,14 @@ export class ScheduleComponent implements OnInit {
   day1_s17 = [];
   day1_s20 = [];
   day1_s21 = [];
-  day1_s22 = [];
+  day1_s23 = [];
   day2_s17 = [];
   day2_s20 = [];
   day2_s21 = [];
-  day2_s22 = [];
+  day2_s23 = [];
   day2_dj18 = []; 
   day2_dj19 = [];
   day2_dj22 = [];
-  testEvent= {
-      "id": "1",
-      "type": "events",
-      "attributes": {
-        "title": "Event #2",
-        "description": "This is event 2",
-        "location": "17th Street Stage",
-        "time": "2000-01-01T12:00:00.000Z",
-        "end_time": "2000-01-01T13:01:00.000Z",
-        "image_thumbnail": null,
-        "image_original": null,
-        "event_date": "Saturday",
-        "date": "2018-04-25T21:10:19.293Z"
-      }
-  };
-  testEvent_sunday= {
-      "id": "1",
-      "type": "events",
-      "attributes": {
-        "title": "Event #2",
-        "description": "This is event 2",
-        "location": "17th Street Stage",
-        "time": "2000-01-01T12:00:00.000Z",
-        "end_time": "2000-01-01T13:01:00.000Z",
-        "image_thumbnail": null,
-        "image_original": null,
-        "event_date": "Sunday",
-        "date": "2018-04-25T21:10:19.293Z"
-      }
-  };
 
   constructor(
     private _api: ApiService
@@ -63,11 +33,7 @@ export class ScheduleComponent implements OnInit {
       // Pulls Events from DB
       this.events = data['data'];
       this.sortEvents();
-      this.day1_s17.push(this.testEvent);
-      this.day2_s17.push(this.testEvent_sunday);
     })
-    this.day1_s17.push(this.testEvent);
-      this.day2_s17.push(this.testEvent_sunday);
     
 
   }
@@ -76,11 +42,11 @@ export class ScheduleComponent implements OnInit {
     this.day1_s17 = this.events.filter((event) => event.attributes.event_date == "Saturday" && event.attributes.location == "17th Street Stage");
     this.day1_s20 = this.events.filter((event) => event.attributes.event_date == "Saturday" && event.attributes.location == "20th Street Stage");
     this.day1_s21 = this.events.filter((event) => event.attributes.event_date == "Saturday" && event.attributes.location == "21st Street Stage");
-    this.day1_s22 = this.events.filter((event) => event.attributes.event_date == "Saturday" && event.attributes.location == "22nd Street Stage");
+    this.day1_s23 = this.events.filter((event) => event.attributes.event_date == "Saturday" && event.attributes.location == "23rd Street Stage");
     this.day2_s17 = this.events.filter((event) => event.attributes.event_date == "Sunday" && event.attributes.location == "17th Street Stage");
     this.day2_s20 = this.events.filter((event) => event.attributes.event_date == "Sunday" && event.attributes.location == "20th Street Stage");
     this.day2_s21 = this.events.filter((event) => event.attributes.event_date == "Sunday" && event.attributes.location == "21st Street Stage");
-    this.day2_s22 = this.events.filter((event) => event.attributes.event_date == "Sunday" && event.attributes.location == "22nd Street Stage");
+    this.day2_s23 = this.events.filter((event) => event.attributes.event_date == "Sunday" && event.attributes.location == "23rd Street Stage");
     this.day2_dj18 = this.events.filter((event) => event.attributes.event_date == "Sunday" && event.attributes.location == "18th Street DJ Booth");
     this.day2_dj19 = this.events.filter((event) => event.attributes.event_date == "Sunday" && event.attributes.location == "19th Street DJ Booth");
     this.day2_dj22 = this.events.filter((event) => event.attributes.event_date == "Sunday" && event.attributes.location == "22nd Street DJ Booth");    
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://54.149.62.148/api',
+  apiUrl: 'http://35.166.67.156/api',
 
   mapbox: {
-    accessToken: 'pk.eyJ1IjoiY29keWVhdHdvcmxkIiwiYSI6ImNpamE3Zms2YzAwNWZ1bWx4bjJpZHhvZG0ifQ.alYL4sUGvCwHzyAQXB8RYQ'
+    accessToken: 'pk.eyJ1IjoiY2FybmF2YWxzZiIsImEiOiJjamd3cDVkaTgwdmNpMzNvamdrYnY1MGQ1In0.2f9fYsJszC8PGTO0lFC-Aw'
   }
 };
