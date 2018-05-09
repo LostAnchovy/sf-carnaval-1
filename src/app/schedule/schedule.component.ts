93b93bc9a178581a7fb532e8ade1eb74aeae8ca3
dbellonzi <dbellonzi@aol.com>
2018-05-08 20:14:42 -0700

    Deleted demo events

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -21,36 +21,6 @@ export class ScheduleComponent implements OnInit {
   day2_dj18 = []; 
   day2_dj19 = [];
   day2_dj22 = [];
-  testEvent= {
-      "id": "1",
-      "type": "events",
-      "attributes": {
-        "title": "Event #2",
-        "description": "This is event 2",
-        "location": "17th Street Stage",
-        "time": "2000-01-01T12:00:00.000Z",
-        "end_time": "2000-01-01T13:01:00.000Z",
-        "image_thumbnail": null,
-        "image_original": null,
-        "event_date": "Saturday",
-        "date": "2018-04-25T21:10:19.293Z"
-      }
-  };
-  testEvent_sunday= {
-      "id": "1",
-      "type": "events",
-      "attributes": {
-        "title": "Event #2",
-        "description": "This is event 2",
-        "location": "17th Street Stage",
-        "time": "2000-01-01T12:00:00.000Z",
-        "end_time": "2000-01-01T13:01:00.000Z",
-        "image_thumbnail": null,
-        "image_original": null,
-        "event_date": "Sunday",
-        "date": "2018-04-25T21:10:19.293Z"
-      }
-  };
 
   constructor(
     private _api: ApiService
@@ -63,11 +33,7 @@ export class ScheduleComponent implements OnInit {
       // Pulls Events from DB
       this.events = data['data'];
       this.sortEvents();
-      this.day1_s17.push(this.testEvent);
-      this.day2_s17.push(this.testEvent_sunday);
     })
-    this.day1_s17.push(this.testEvent);
-      this.day2_s17.push(this.testEvent_sunday);
     
 
   }
