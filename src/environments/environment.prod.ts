4a6f4f7bfdc9a9bdc32b6d74da8d07a129e51bc4
dbellonzi <dbellonzi@aol.com>
2018-05-11 18:42:23 -0700

    api adjustments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'api.carnavalsanfrancisco.org/api',
+  apiUrl: 'https://api.carnavalsanfrancisco.org/api',
 
   mapbox: {
     accessToken: 'pk.eyJ1IjoiY2FybmF2YWxzZiIsImEiOiJjamd3cDVkaTgwdmNpMzNvamdrYnY1MGQ1In0.2f9fYsJszC8PGTO0lFC-Aw'
