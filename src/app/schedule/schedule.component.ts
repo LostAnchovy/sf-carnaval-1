7b997cc3887c56b879f9662bc45c87e6730423ce
dbellonzi <dbellonzi@aol.com>
2018-05-16 10:16:23 -0700

    Added Main Stage

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -13,10 +13,12 @@ export class ScheduleComponent implements OnInit {
   day1_s17 = [];
   day1_s20 = [];
   day1_s21 = [];
+  day1_s22 = [];
   day1_s23 = [];
   day2_s17 = [];
   day2_s20 = [];
   day2_s21 = [];
+  day2_s22 = [];
   day2_s23 = [];
   day2_dj18 = []; 
   day2_dj19 = [];
@@ -42,10 +44,12 @@ export class ScheduleComponent implements OnInit {
     this.day1_s17 = this.events.filter((event) => event.attributes.event_date == "Saturday" && event.attributes.location == "17th Street Stage");
     this.day1_s20 = this.events.filter((event) => event.attributes.event_date == "Saturday" && event.attributes.location == "20th Street Stage");
     this.day1_s21 = this.events.filter((event) => event.attributes.event_date == "Saturday" && event.attributes.location == "21st Street Stage");
+    this.day1_s22 = this.events.filter((event) => event.attributes.event_date == "Saturday" && event.attributes.location == "Main Stage: 22nd Street Stage");
     this.day1_s23 = this.events.filter((event) => event.attributes.event_date == "Saturday" && event.attributes.location == "23rd Street Stage");
     this.day2_s17 = this.events.filter((event) => event.attributes.event_date == "Sunday" && event.attributes.location == "17th Street Stage");
     this.day2_s20 = this.events.filter((event) => event.attributes.event_date == "Sunday" && event.attributes.location == "20th Street Stage");
     this.day2_s21 = this.events.filter((event) => event.attributes.event_date == "Sunday" && event.attributes.location == "21st Street Stage");
+    this.day2_s22 = this.events.filter((event) => event.attributes.event_date == "Sunday" && event.attributes.location == "Main Stage: 22nd Street Stage");
     this.day2_s23 = this.events.filter((event) => event.attributes.event_date == "Sunday" && event.attributes.location == "23rd Street Stage");
     this.day2_dj18 = this.events.filter((event) => event.attributes.event_date == "Sunday" && event.attributes.location == "18th Street DJ Booth");
     this.day2_dj19 = this.events.filter((event) => event.attributes.event_date == "Sunday" && event.attributes.location == "19th Street DJ Booth");
